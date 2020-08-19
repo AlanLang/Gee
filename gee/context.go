4291f08acfa399e717e831f4ecf440d4084d4087
alan <alan@fanruan.com>
2020-08-19 09:36:28 +0800

    feat: 前缀路由树

--- gee/context.go
+++ gee/context.go
@@ -17,8 +17,15 @@ type Context struct {
 	// request info
 	Path   string
 	Method string
+	Params map[string]string
 	// response info
 	StatusCode int
+}
+
+// Param Param
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
 }
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
